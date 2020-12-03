29c6c387ddedc497c2e81195c032b5568d6b9e0a
Mehmet Denizhan Erdem <denizhanerdem@gmail.com>
2020-12-03 12:02:22 +0300

    day03: initial version

---
+++ day03/src/main.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+#[derive(PartialEq, Debug)]
+enum Space {
+    Free,
+    Tree,
+}
+
+fn main() {
+    let reader = BufReader::new(File::open("input.txt").unwrap());
+    let space = reader
+        .lines()
+        .map(|x| {
+            x.unwrap()
+                .chars()
+                .map(|x| match x {
+                    '.' => Space::Free,
+                    '#' => Space::Tree,
+                    _ => Space::Free,
+                })
+                .collect::<Vec<Space>>()
+        })
+        .collect::<Vec<Vec<Space>>>();
+
+    println!("day01: {:?}", find_trees_on_slope(&space, &(3, 1)));
+    println!(
+        "day02: {:?}",
+        [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+            .iter()
+            .map(|x| find_trees_on_slope(&space, x))
+            .fold(1, |acc, x| x * acc)
+    );
+    
+}
+
+fn find_trees_on_slope(space: &Vec<Vec<Space>>, slope: &(usize, usize)) -> usize {
+    space
+        .iter()
+        .step_by(slope.1)
+        .fold((0, 0), |(pos, total), x| {
+            let tree: usize = if *x.get(pos % x.len()).unwrap() == Space::Tree {
+                1
+            } else {
+                0
+            };
+            (pos + slope.0, total + tree)
+        })
+        .1
+}
